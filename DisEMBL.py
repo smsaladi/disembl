a053695266adf815080b272ccea7e7b2c7d76723
Shyam Saladi <saladi@caltech.edu>
2016-06-09 13:15:03 -0700

    added argparse as well as rewrite of SavitzkyGolay filter (omits needing to use TISEAN)

--- DisEMBL.py
+++ DisEMBL.py
@@ -1,30 +1,45 @@
-#!/usr/local/bin/python
-# Copyright (C) 2004 Rune Linding & Lars Juhl Jensen - EMBL
-# The DisEMBL is licensed under the GPL license
-# (http://www.opensource.org/licenses/gpl-license.php)
-# DisEMBL pipeline
+#!/usr/bin/env python
+"""
+This version of DisEMBL represents a complete rewriting of the code. The regions
+identified as disordered do not change.
 
-# Modified to work with current versions of Biopython (1.7+)
-# by Shyam Saladi (saladi1@illinois.edu), Janauary 2013 
-# Bio:SeqIO completely replaces Bio:Fasta
+Much of the calculation is done with NumPy/Scipy instead of previously written,
+custom C code since it ends up being faster and significantly simplifies
+maintainance.
 
-from string import *
-from sys import argv
-from Bio import File
-from Bio import SeqIO
+The original calculation will be available via '--old' but will be removed in
+further versions.
+
+Authors:
+
+Originally -
+Copyright (C) 2004 Rune Linding & Lars Juhl Jensen - EMBL
+
+Rewrite (2.0+) -
+Copyright (C) 2016 Shyam Saladi - Caltech
+
+
+The DisEMBL is licensed under the GNU General Public License
+(http://www.opensource.org/licenses/gpl-license.php)
+"""
+
 import fpformat
 import sys
 import tempfile
+from os import system, popen3
+from string import *
+
 import os
-from os import system,popen3
+import argparse
 
-# change these to the correct paths
-NN_bin = '/PATH/DisEMBL-1.4/disembl'
-SG_bin = '/PATH/DisEMBL-1.4/sav_gol'
+import pandas as pd
+import scipy.signal
+import Bio.SeqIO
 
-def JensenNet(sequence):
+
+def JensenNet(sequence, NN_bin='~/DisEMBL-1.4/disembl'):
     outFile = tempfile.mktemp()
-    inFile= tempfile.mktemp()
+    inFile = tempfile.mktemp()
     open(inFile,'w').write(sequence+'\n')
     system(NN_bin + '< ' + inFile +' > ' + outFile)
     REM465 = []
@@ -45,7 +60,7 @@ def JensenNet(sequence):
     return COILS, HOTLOOPS, REM465
 
 
-def SavitzkyGolay(window,derivative,datalist):
+def SavitzkyGolay(window, derivative, datalist, SG_bin='~/DisEMBL-1.4/sav_gol'):
     if len(datalist) < 2*window:
         window = len(datalist)/2
     elif window == 0:
@@ -67,6 +82,7 @@ def SavitzkyGolay(window,derivative,datalist):
         else:
             SG_results.append(f)
     return SG_results
+
 
 def getSlices(NNdata, fold, join_frame, peak_frame, expect_val):
     slices = []
@@ -121,6 +137,7 @@ def reportSlicesTXT(slices, sequence):
     print ''
     print s
 
+    return
 
 
 def runDisEMBLpipeline():
@@ -150,8 +167,8 @@ def runDisEMBLpipeline():
     print '# Copyright (C) 2004 - Rune Linding & Lars Juhl Jensen '
     print '# EMBL Biocomputing Unit - Heidelberg - Germany        '
     print '#'
-    for cur_record in SeqIO.parse(db, "fasta"):
-	    sequence = upper(str(cur_record.seq.data))
+    for cur_record in Bio.SeqIO.parse(db, "fasta"):
+	    sequence = upper(str(cur_record.seq))
             # Run NN
             COILS_raw, HOTLOOPS_raw, REM465_raw = JensenNet(sequence)
             # Run Savitzky-Golay
@@ -174,6 +191,223 @@ def runDisEMBLpipeline():
                 sys.stderr.write('Wrong mode given: '+mode+'\n')
                 raise SystemExit
     db.close()
+
     return
 
-runDisEMBLpipeline()
+
+def calc_disembl(protseq, smooth_frame,
+                 coils=True, hotloops=True, rem465=True, old_filter=False):
+    """Calculate the output of the DisEMBL neural network for a sequence
+
+    Parameters
+    ----------
+    protseq : str
+        Protein sequence for which to calculate DisEMBL scores
+
+    smooth_frame : int,
+        Number of residues forward and backward to smooth other (window size
+        would be smooth_frame * 2 + 1)
+
+    coils : Optional[bool]
+        Whether to provide coils output
+
+    hotloops : Optional[bool]
+        Whether to provide hotloops output
+
+    rem465 : Optional[bool]
+        Whether to provide rem465 output
+
+    old_filter : Optional[bool]
+        The old filter (from TISEAN) simply replaces the first [smooth_frame]
+            and last [smooth_frame] values with the raw calcuations
+
+    Returns
+    -------
+    pd.DataFrame
+        Columns corresponding to the sequence as well as the properties
+        specified (e.g. hotloops, coils)
+
+    Raises
+    ------
+    None
+    """
+
+    protseq = upper(protseq)
+    COILS_raw, HOTLOOPS_raw, REM465_raw = JensenNet(protseq)
+
+    pred = {'sequence': protseq}
+
+    if coils:
+        pred['coils'] = scipy.signal.savgol_filter(COILS_raw,
+                            window_length=smooth_frame*2+1,
+                            polyorder=2, deriv=0, mode='interp')
+        if old_filter:
+            pred['coils'][:smooth_frame] = REM465_raw[:smooth_frame]
+            pred['coils'][-smooth_frame:] = REM465_raw[-smooth_frame:]
+
+    if hotloops:
+        pred['hotloops'] = scipy.signal.savgol_filter(HOTLOOPS_raw,
+                            window_length=smooth_frame*2+1,
+                            polyorder=2, deriv=0, mode='interp')
+        if old_filter:
+            pred['hotloops'][:smooth_frame] = REM465_raw[:smooth_frame]
+            pred['hotloops'][-smooth_frame:] = REM465_raw[-smooth_frame:]
+
+    if rem465:
+        pred['rem465'] = scipy.signal.savgol_filter(REM465_raw,
+                            window_length=smooth_frame*2+1,
+                            polyorder=2, deriv=0, mode='interp')
+        if old_filter:
+            pred['rem465'][:smooth_frame] = REM465_raw[:smooth_frame]
+            pred['rem465'][-smooth_frame:] = REM465_raw[-smooth_frame:]
+
+    return pd.DataFrame(pred)
+
+
+def main():
+    # set up and parse arguments
+    parser = argparse.ArgumentParser(
+        description='Calculate DisEMBL disorder prediction.',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('inputfile',
+        type=str,
+        default=sys.stdin,
+        help='File of protein coding sequences to predict on.')
+
+    parser.add_argument('--inputformat',
+        type=str,
+        default='fasta',
+        help='File format of [protein_seq]. Any Bio.SeqIO-readable format is supported')
+
+    parser.add_argument('--smooth_frame',
+        type=int,
+        default=8,
+        help='smooth_frame (window length = smooth_frame * 2 + 1)')
+
+    parser.add_argument('--peak_frame',
+        type=int,
+        default=8,
+        help='peak_frame')
+
+    parser.add_argument('--join_frame',
+        type=int,
+        default=4,
+        help='join_frame')
+
+    parser.add_argument('--fold_coils',
+        type=float,
+        default=1.2,
+        help='fold_coils')
+
+    parser.add_argument('--fold_hotloops',
+        type=float,
+        default=1.4,
+        help='fold_hotloops')
+
+    parser.add_argument('--fold_rem465',
+        type=float,
+        default=1.2,
+        help='fold_rem465')
+
+    parser.add_argument('--expect_coils',
+        type=float,
+        default=0.43,
+        help='expect_coils')
+
+    parser.add_argument('--expect_hotloops',
+        type=float,
+        default=0.086,
+        help='expect_hotloops')
+
+    parser.add_argument('--expect_rem465',
+        type=float,
+        default=0.50,
+        help='expect_rem465')
+
+    parser.add_argument('--new_filter',
+        action='store_true',
+        help="Don't simply replace the first [smooth_frame] and last "
+             "[smooth_frame] values. "
+             "Use 'interp' from scipy.signal.savgol_filter")
+
+    parser.add_argument('--mode',
+        type=str,
+        choices=['default', 'scores'],
+        default='default',
+        help='mode: default or scores which will give scores per residue'
+             'in TAB seperated format')
+
+    parser.add_argument('--quiet',
+        action='store_true',
+        help='Supress printing the DisEMBL banner')
+
+    parser.add_argument('--old',
+        action='store_true',
+        help='Completely run the old code')
+
+    args = parser.parse_args()
+
+    if args.old:
+        sys.argv = [sys.argv[0],
+                    args.smooth_frame, args.peak_frame,
+                    args.join_frame, args.fold_coils,
+                    args.fold_hotloops, args.fold_rem465,
+                    args.inputfile, args.mode]
+
+        runDisEMBLpipeline()
+        return
+
+
+    if not args.quiet:
+        print "\n".join(
+             (' ____  _     _____ __  __ ____  _     ',
+              '|  _ \(_)___| ____|  \/  | __ )| |    ',
+              '| | | | / __|  _| | |\/| |  _ \| |    ',
+              '| |_| | \__ \ |___| |  | | |_) | |___ ',
+              '|____/|_|___/_____|_|  |_|____/|_____|',
+              '# ',
+              '# Original:',
+              '# Copyright (C) 2004 - Rune Linding & Lars Juhl Jensen',
+              '# EMBL Biocomputing Unit - Heidelberg - Germany',
+              '# ',
+              '# Rewrite (v2.0+):',
+              '# Copyright (C) 2016 - Shyam Saladi',
+              '# California Institute of Technology - Pasadena - CA - USA',
+              '# ')) #, sep="\n", file=sys.stderr)
+
+    for record in Bio.SeqIO.parse(args.inputfile, args.inputformat):
+        sequence = upper(str(record.seq))
+
+        preds = calc_disembl(sequence, args.smooth_frame)
+
+        if args.mode == 'default':
+            sys.stdout.write('> ' + record.id + '_COILS ')
+            reportSlicesTXT(getSlices(preds.coils.tolist(),
+                                      args.fold_coils,
+                                      args.join_frame,
+                                      args.peak_frame,
+                                      args.expect_coils), sequence)
+            sys.stdout.write('> ' + record.id + '_REM465 ')
+            reportSlicesTXT(getSlices(preds.coils.tolist(),
+                                      args.fold_rem465,
+                                      args.join_frame,
+                                      args.peak_frame,
+                                      args.expect_rem465), sequence)
+            sys.stdout.write('> ' + record.id + '_HOTLOOPS ')
+            reportSlicesTXT(getSlices(preds.coils.tolist(),
+                                      args.fold_hotloops,
+                                      args.join_frame,
+                                      args.peak_frame,
+                                      args.expect_hotloops), sequence)
+            print('\n')
+        elif args.mode == 'scores':
+            # Format header row as close to original DisEMBL as reasonable
+            preds.rename(columns={"sequence": "residue"}, inplace=True)
+            preds.columns = [upper(x) for x in preds.columns]
+            print(preds.to_csv(sep="\t", index=False))
+    return
+
+
+if __name__ == '__main__':
+    main()
