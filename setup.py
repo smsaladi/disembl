f150a919e67bdb4755511018b8c9cb607e8ec6c9
Shyam Saladi <saladi@caltech.edu>
2016-07-02 12:10:29 -0700

    files in preparation for PyPI

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'disembl',
+  packages = ['disembl'],
+  version = '2.0',
+  description = 'A refactoring/reimplementation of DisEMBL',
+  author = 'Shyam Saladi',
+  author_email = 'saladi@caltech.edu',
+  url = 'https://github.com/smsaladi/disembl',
+  download_url = 'https://github.com/smsaladi/disembl/tarball/2.0',
+  keywords = ['protein', 'disorder', 'sequence', 'bioinformatics'],
+  classifiers = [],
+)
