d72d38ebe6948c3daebc577e129bf13f747e290a
Nadine Bradbury <bradbury@caltech.edu>
2016-08-28 22:05:27 -0700

    cleaned up setup.py more

--- setup.py
+++ setup.py
@@ -4,12 +4,11 @@ from setuptools.command.test import test as TestCommand
 class PyTest(TestCommand):
     def finalize_options(self):
         TestCommand.finalize_options(self)
-        self.test_args = []
         self.test_suite = True
     def run_tests(self):
         #import here, cause outside the eggs aren't loaded
         import pytest
-        pytest.main(self.test_args)
+        pytest.main()
 
 setup(
   name='disembl',
